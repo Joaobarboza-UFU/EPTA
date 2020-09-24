374abab0711b7df74beaa405b7e27bc5e36b1875
João Barboza Rodrigues <joaobarboza.ufu@gmail.com>
2020-09-24 18:25:06 -0300

    Update Epta_Rocket.py

--- Epta_Rocket.py
+++ Epta_Rocket.py
@@ -1,1 +1,4 @@
+# Code that reads a CSV file containing the information about the trajectory of a rocket
+# Joao Barboza Rodrigues
+# Computer Engineering student at Federal University of Uberlandia
 
