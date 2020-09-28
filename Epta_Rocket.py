8897b6b10e730a89facfd522ab4b667b53824d19
João Barboza Rodrigues <joaobarboza.ufu@gmail.com>
2020-09-28 10:48:57 -0300

    Update Epta_Rocket.py

--- Epta_Rocket.py
+++ Epta_Rocket.py
@@ -1,12 +1,3 @@
-# Code that reads a CSV file containing the information about the trajectory of a rocket
-# Joao Barboza Rodrigues
-# Computer Engineering student at Federal University of Uberlandia
-
-import codecs
-import csv
-import matplotlib.pyplot as plt
-import numpy as np
-
 import codecs
 import csv
 import matplotlib.pyplot as plt
@@ -32,17 +23,27 @@ x_data = []
 y_data = []
 z_data = []
 verticle_speed = []
+accelle = []
+grounds = []
+time = []
+distancia = []
 
 for i in range(266):
     x_data.append(float(list_data[i]['North'][0]))
     y_data.append(float(list_data[i]['East'][0]))
     z_data.append(float(list_data[i]['Altitude'][0]))
     verticle_speed.append(float(list_data[i]["Verticle Speed"][0]))
+    accelle.append(float(list_data[i]["Accelleration"][0]))
+    grounds.append(float(list_data[i]["Ground speed"][0]))
+    distancia.append(float(list_data[i]["Distance Travelled"][0]))
+    time.append(float(list_data[i]["time"][0]))
     
     
 ax = plt.axes(projection='3d')
 ax.set_title('Trajetória foguete');
 ax.scatter(x_data, y_data, z_data, c=x_data, cmap='viridis', linewidth=0.5);
+ax.view_init(30,30)
+
 
 fig= plt.figure(figsize=(6,3))
 axes= fig.add_axes([0.1,0.1,0.8,0.8])
@@ -56,6 +57,22 @@ fig2= plt.figure(figsize=(6,3))
 axes2= fig2.add_axes([0.1,0.1,0.8,0.8])
 axes2.set_title('Eixo x e y ( Visão de cima )');
 axes2.plot(x_data,y_data)
+fig3= plt.figure(figsize=(6,3))
+axes3= fig3.add_axes([0.1,0.1,0.8,0.8])
+axes3.set_title('Velocidade vertical no tempo');
+axes3.plot(time,verticle_speed)
+fig4= plt.figure(figsize=(6,3))
+axes4= fig4.add_axes([0.1,0.1,0.8,0.8])
+axes4.set_title('Aceleração no tempo ( Gs)');
+axes4.plot(time,accelle)
+fig5= plt.figure(figsize=(6,3))
+axes5= fig5.add_axes([0.1,0.1,0.8,0.8])
+axes5.set_title('Velocidade em solo no tempo');
+axes5.plot(time,grounds)
+fig6= plt.figure(figsize=(6,3))
+axes6= fig6.add_axes([0.1,0.1,0.8,0.8])
+axes6.set_title('Distancia no tempo');
+axes6.plot(time,distancia)
 
 
 plt.show()
