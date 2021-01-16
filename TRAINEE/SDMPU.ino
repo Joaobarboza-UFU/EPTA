ff2af3f9ec9bee109a78f1a289a4c5141881d0d8
João Barboza Rodrigues <joaobarboza.ufu@gmail.com>
2021-01-15 23:10:31 -0300

    Used base MPU code and store info in SD card

---
+++ TRAINEE/SDMPU.ino
@@ -0,0 +1,56 @@
+#include <SD.h>
+#include <SPI.h>
+#include <MPU9250_asukiaaa.h>
+
+MPU9250_asukiaaa mySensor;
+
+#ifdef ESP32_HAL_I2C_H
+#define SDA_PIN 21
+#define SCL_PIN 22
+#endif
+
+void setup(){
+  Serial.begin(115200);
+  Serial.println("Inicializing SD...");
+  if (!SD.begin()) {
+    Serial.println("Erro, verifique se o cartão está inserido, reiniciando o sistema...");
+    delay(5000);
+    ESP.restart();
+  }
+  Serial.println("success.");
+  #ifdef ESP32_HAL_I2C_H
+  // for esp32
+  Wire.begin(SDA_PIN, SCL_PIN); //sda, scl
+  #else
+  Wire.begin();
+  #endif
+   
+  mySensor.setWire(&Wire);
+  mySensor.beginAccel();
+  mySensor.beginMag();
+ 
+}
+void loop() {
+ 
+  File dataFile = SD.open("MPU.txt", FILE_WRITE);
+
+  if (dataFile) {
+    Serial.println("file open!");
+ 
+      dataFile.print("Acelerometro: ");
+      dataFile.print(" | ");
+      dataFile.print("X" + String(mySensor.accelX())+ "Y" + String(mySensor.accelY())+ "Z" + String(mySensor.accelZ()));
+      dataFile.print("Magnetómetro: ");
+      dataFile.print(" | ");
+      dataFile.print("X" + String(mySensor.magX())+ "Y" + String(mySensor.accelY())+ "Z" + String(mySensor.accelZ()));
+      dataFile.println("");
+      dataFile.close();
+  }
+
+  else {
+    Serial.println("fail to open File");
+  }
+ 
+  delay(2000);
+ 
+}
